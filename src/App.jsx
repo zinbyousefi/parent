fa098404aa4ef8eaf95667a35e263afd434dc039
Zeinab <yousefizeinb@gmail.com>
2025-02-03 13:52:39 +0330

    feat: :zap: home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="text-red-500">hiiii</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
